db9cdc4777e85d233839d2c1376dad1be8b117c8
lunajulio <lunajulio@uninorte.edu.co>
2024-06-28 23:43:23 -0500

    Mensaje del commit

---
+++ src/main/java/com/alura/LiteraturA/LiteraturAApplication.java
@@ -0,0 +1,13 @@
+package com.alura.LiteraturA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/LiteraturA/LiteraturAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.LiteraturA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
